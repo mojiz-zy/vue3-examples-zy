971601e4530ba6b58e96d1dde1bc66a47e8ee660
ziyue111 <1004471534@qq.com>
2021-04-01 10:05:40 +0800

    7-8

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,3 @@
-import component from "*.vue";
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
@@ -15,6 +14,7 @@ const routes: Array<RouteRecordRaw> = [
     path: "/example03",
     component: () => import("@/views/example03/Example03.vue")
   },
+
   {
     props: true,
     path: "/example03-sid/studentid/:sid/homeworkid/:hid", //:sid  通过 :  来获取参数
@@ -22,8 +22,48 @@ const routes: Array<RouteRecordRaw> = [
   },
   {
     props: true,
-    path: "/example04",
-    component: () => import("@/views/example04/Exmple04-01.vue")
+    path: "/example04-01",
+    component: () => import("@/views/example04/Example04-01.vue")
+  },
+  {
+    props: true,
+    path: "/example04-02",
+    component: () => import("@/views/example04/Example04-02.vue")
+  },
+  {
+    props: true,
+    path: "/example06-01",
+    component: () => import("@/views/example06/Example06-01.vue")
+  },
+  {
+    props: true,
+    path: "/example06-02",
+    component: () => import("@/views/example06/Example06-02.vue")
+  },
+  {
+    props: true,
+    path: "/example07-01",
+    component: () => import("@/views/example07/Example07-01.vue")
+  },
+  {
+    props: true,
+    path: "/example07-02",
+    component: () => import("@/views/example07/Example07-02.vue")
+  },
+  {
+    props: true,
+    path: "/example07-03",
+    component: () => import("@/views/example07/Example07-03.vue")
+  },
+  {
+    props: true,
+    path: "/example07-04",
+    component: () => import("@/views/example07/Example07-04.vue")
+  },
+  {
+    props: true,
+    path: "/example08-01",
+    component: () => import("@/views/example07/Example08-01.vue")
   }
 ];
 
